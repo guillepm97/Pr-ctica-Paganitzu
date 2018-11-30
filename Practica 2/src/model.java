4564b53f15a9a6f627a25b709c563675a99dd849
Guillermo <100346149@alumnos.uc3m.es>
2018-11-30 10:45:50 +0100

    Ultima version

--- Practica 2/src/model.java
+++ Practica 2/src/model.java
@@ -1,5 +1,11 @@
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.JOptionPane;
+
 import org.jacop.core.BooleanVar;
 import org.jacop.core.Store;
 import org.jacop.jasat.utils.structures.IntVec;
@@ -16,6 +22,7 @@ public class model{
 	
 	public static void main(String args[]){
 		
+		
 		 Store store = new Store();
 		  SatWrapper satWrapper = new SatWrapper();
 		  store.impose(satWrapper);					/* Importante: sat problem */
@@ -25,6 +32,7 @@ public class model{
 		  int numFilas=miLaberinto.numFilas();
 		  int numColumnas=miLaberinto.numColumnas();
 		  mapa=miLaberinto.cargarMapa();
+		 
 		  
 		  
 		  
@@ -35,13 +43,13 @@ public class model{
 		  BooleanVar[][] A = new BooleanVar[miLaberinto.filas][miLaberinto.columnas];
 		  BooleanVar[][] S = new BooleanVar[miLaberinto.filas][miLaberinto.columnas];
 		  
-		  BooleanVar[] variableArray = new BooleanVar[2];
+		  
 		  int [][] ALiteral = new int [miLaberinto.filas][miLaberinto.columnas];
 		  int [][] SLiteral = new int [miLaberinto.filas][miLaberinto.columnas];
 		  
 		  int contadorVariables=0;
-		  for(int i=0; i<mapa.length;i++){
-			    for(int j=0;j<mapa[i].length;j++){
+		  for(int i=0; i<numFilas;i++){
+			    for(int j=0;j<numColumnas;j++){
 
 			      if(mapa[i][j]==' ' ){
 			    	
@@ -53,31 +61,33 @@ public class model{
 					
 					ALiteral[i][j] = satWrapper.cpVarToBoolVar(A[i][j], 1, true);
 					SLiteral[i][j] = satWrapper.cpVarToBoolVar(S[i][j], 1, true);
-					addClause(satWrapper,ALiteral[i][j],SLiteral[i][j]); //Al o la serpiente sólo pueden estar en casillas vacías.
+					
 			      }
 			      
 			   
 			    }
 			  }
+		  
+		
 		  int contador=0;
 		  BooleanVar[] allVariables = new BooleanVar[contadorVariables];
 		  
-		  for(int i=0; i<mapa.length;i++){
-			    for(int j=0;j<mapa[i].length;j++){
+		  for(int i=0; i<numFilas;i++){
+			    for(int j=0;j<numColumnas;j++){
 			    	
-			    	if(A[i][j]!=null && contador< contadorVariables/2) {
+			    	if(A[i][j]!=null ) {
 			    		allVariables[contador]= A[i][j];
 				    	contador++;
 			    	}
 			    	
 			    }
 		  }
+		 
 		  
-		  int contador2=contadorVariables/2;
-		  for(int i=0; i<mapa.length;i++){
-			    for(int j=0;j<mapa[i].length;j++){
+		  for(int i=0; i<numFilas;i++){
+			    for(int j=0;j<numColumnas;j++){
 			    	
-			    	if(A[i][j]!=null && contador<= contadorVariables) {
+			    	if(S[i][j]!=null) {
 			    		allVariables[contador]= S[i][j];
 				    	contador++;
 			    	}
@@ -85,12 +95,31 @@ public class model{
 			    }
 		  }
 		  
+		  
+		  
+		//Al o la serpiente sólo pueden estar en casillas vacías.
+		 /* for(int i=0; i<numFilas;i++){
+			    for(int j=0;j<numColumnas;j++){
+			      if(mapa[i][j]==' ' ){
+			    	  addClause(satWrapper,-ALiteral[i][j],-SLiteral[i][j]); //A->-S
+			    	  addClause(satWrapper,-SLiteral[i][j],-ALiteral[i][j]); //S->-A
+			    	 
+			    	  
+			      	}
+			    }
+		  }
+		 
+		
+		  
 		  //Las serpientes no pueden estar en la misma fila
-		  for(int i=0; i<mapa.length;i++){
-			    for(int j=0;j<mapa[i].length ;j++){
-			    	for(int k=0; k<mapa[i].length;i++){
-			    		if(j!=k)
-			    		addClause(satWrapper,-SLiteral[i][j],SLiteral[i][k]);
+		  for(int i=0; i<numFilas;i++){
+			    for(int j=0;j<numColumnas ;j++){
+			    	for(int k=0; k<numColumnas;k++){
+			    		if(j!=k) {
+			    		addClause(satWrapper,-SLiteral[i][j],-SLiteral[i][k]);
+			    		
+			    		
+			    		}
 			    		
 			    	}
 			    	
@@ -98,7 +127,38 @@ public class model{
 		  }
 		  
 		  
+		  //Las serpientes no pueden estar en la misma fila o columna que Al
+		  for(int i=0; i<numFilas;i++){
+			    for(int j=0;j<numColumnas ;j++){
+			    	for(int k=0; k<numColumnas;k++){
+			    	for(int z=0; z<numFilas;z++) {
+			    		if(j!=k || z!=i )
+			    		addClause(satWrapper,-SLiteral[i][j],-ALiteral[z][k]);
+			    		addClause(satWrapper,-ALiteral[i][j],-SLiteral[z][k]);
+			    		
+			    		}
+			    	}
+			    	
+			    }
+		  }*/		  
 		 
+		  //Solo puede haber un Al
+		  for(int i=0; i<numFilas;i++){
+			    for(int j=0;j<numColumnas ;j++){
+			    	for(int k=0; k<numColumnas;k++){
+			    	for(int z=0; z<numFilas;z++) {
+			    		if(j!=k && z!=i ) { 
+			    		addClause(satWrapper,-ALiteral[i][j],-ALiteral[z][k]);
+			    		addClause(satWrapper,ALiteral[i][j],ALiteral[z][k]);
+			    		}
+			    	}
+			    	}
+			    	
+			    }
+		  }		  
+		 
+		  
+		  
 		  
 		
 			// 4. INVOCAR AL SOLUCIONADOR
@@ -111,8 +171,28 @@ public class model{
 			if (result) {
 				System.out.println("Solution: ");
 				
-				//System.out.println(A.id() + " " + A.value());
-				//System.out.println(S.id() + " " + S.value());
+				for(int i=0;i<numFilas;i++) {
+					for(int j=0;j<numColumnas;j++) {
+						
+						if( A[i][j]!=null && A[i][j].value()==1) {
+							
+							mapa[i][j]='A';
+							
+						}
+						if(A[i][j]!=null && S[i][j].value()==1) {
+							mapa[i][j]='S';
+						}
+					}
+				}
+				
+				for(int i=0;i<numFilas;i++) {
+					for(int j=0;j<numColumnas;j++) {
+						
+						System.out.print(mapa[i][j]+ " ");
+					}
+					System.out.println();
+				}
+				
 				
 			} else{
 				System.out.println("*** No solution");
